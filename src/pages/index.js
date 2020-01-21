a9847761ea91808e1432375b7365e184adf24550
Matthew Farley <matthewthomasfarley@gmail.com>
2020-01-21 00:15:01 -0500

    Replace homepage boilerplate with real content

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,29 +7,32 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "Matthew T Farley"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
           title="Home"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+          keywords={[
+            `personal`,
+            `portfolio`,
+            `blog`,
+            `gatsby`,
+            `javascript`,
+            `react`,
+          ]}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
-        <h1>
-          Hey people{" "}
+        <h2>
+          Hey!&nbsp;
           <span role="img" aria-label="wave emoji">
             👋
           </span>
-        </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
-        <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
-        </p>
-        <p>Now go build something great!</p>
+        </h2>
+        <p>Welcome to my personal website!</p>
+        <p>This site is under construction. Check back for updates!</p>
+        <p>- Matt</p>
         <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
+          <Button marginTop="35px">Blog</Button>
         </Link>
       </Layout>
     )
